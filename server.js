dcea4b99620555e638908d3d90d4b53f78dc2062
Matthew <matthewcnitschke@gmail.com>
2018-01-29 11:00:59 -0700

    added server code

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+
+app.use(express.static('public'))
+
+app.listen(8080);
