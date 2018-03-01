822ae963781edd0d974b38802e384ed3963aec6d
Matthew <matthewcnitschke@gmail.com>
2018-03-01 16:18:59 -0700

    fixed bug in update script

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
-git checkout master # ensure git is on the master branch
 git pull # pull latest changes on master
-pm2 restart server # restart the server.js process
+pm2 restart app # restart the server.js process
+pm2 flush # clear app logs
