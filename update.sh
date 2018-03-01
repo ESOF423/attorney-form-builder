7fbea6b69101c38033d2cf2a7ee3223c5e80f468
Matthew <matthewcnitschke@gmail.com>
2018-03-01 16:23:09 -0700

    added check in update script to ensure it is run as root

--- update.sh
+++ update.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
 pm2 delete app
 git pull # pull latest changes on master
 sudo pm2 start app.js # restart the server.js process
