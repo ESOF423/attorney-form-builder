5dc2657d9f1ad7c3e67a1eb0ad48d6e4a28bd781
Matthew <matthewcnitschke@gmail.com>
2018-03-01 16:20:31 -0700

    fixed another bug in update script

--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
+pm2 delete app
 git pull # pull latest changes on master
-pm2 restart app # restart the server.js process
+sudo pm2 start app.js # restart the server.js process
 pm2 flush # clear app logs
