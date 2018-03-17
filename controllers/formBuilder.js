d81b2a6c9c509be60c7423d840c6a4ea428b20b2
Matthew <matthewcnitschke@gmail.com>
2018-03-17 10:37:14 -0600

    fixed bug in calls to models in formBuilder

--- controllers/formBuilder.js
+++ controllers/formBuilder.js
@@ -2,14 +2,14 @@ const path = require('path')
 const express = require('express')
 const router = express.Router()
 
-const formModel = require('models/form.js')
-const formQuestionModel = require('models/formQuestion.js')
+const formModel = require('../models/form.js')
+const formQuestionModel = require('../models/formQuestion.js')
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../views', 'formBuilder.html'))
 })
 
-router.post('/submitForm', (req, res) => {
+router.post('/submitForm', async (req, res) => {
     if (req.session.isAuthenticated && req.session.isAttorney){
         let {name, cost, state} = req.body
         let questions = JSON.parse(req.body.questions)
