6dc9d7334241f74491cfaca58365ea6dea9dd605
BrendanPTracey <bulldogbrendan101@gmail.com>
2018-04-02 01:15:18 -0600

    Update formTest.js

--- test/formTest.js
+++ test/formTest.js
@@ -1,4 +1,4 @@
-#testing form creation
+//testing form creation
 
 const assert = require('chai').assert;
 const form = require('../form.js');
@@ -9,18 +9,18 @@ describe('Form', get(formId){
 	})
 })
 
-#ensure unique formID is reinforced, should fail
+//ensure unique formID is reinforced, should fail
 describe('Form', create(attorneyId, name, cost, state){
 	it('isn't the same as other ID', get(1234, dave , 40, Montana){
 		assert.equal(get(formId);
 	});
 });
 
-#make sure if searched attorney does not exist nothing is returned
+//make sure if searched attorney does not exist nothing is returned
 describe('Form' getByAttorney(attorneyId, name, cost, state){
 	it('attorney doesn't exist', getByAttorney(1236, chuck, 35, Wyoming){
 		assert.equal(get(formId);
 	});
 });
 
-#placeholder for when I figure out how to deal with unions in this context
+//placeholder for when I figure out how to deal with query unions and exclusions in this context
