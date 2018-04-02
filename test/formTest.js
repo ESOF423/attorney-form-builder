bd9d10c4de7fe381ca5af496e91f45a1d305b17c
BrendanPTracey <bulldogbrendan101@gmail.com>
2018-04-01 21:28:01 -0600

    Add files via upload

---
+++ test/formTest.js
@@ -0,0 +1,26 @@
+#testing form creation
+
+const assert = require('chai').assert;
+const form = require('../form.js');
+
+describe('Form', get(formId){
+	it('should fail, not null', get(formId){
+		assert.typeOf(get(formID, null);
+	})
+})
+
+#ensure unique formID is reinforced, should fail
+describe('Form', create(attorneyId, name, cost, state){
+	it('isn't the same as other ID', get(1234, dave , 40, Montana){
+		assert.equal(get(formId);
+	});
+});
+
+#make sure if searched attorney does not exist nothing is returned
+describe('Form' getByAttorney(attorneyId, name, cost, state){
+	it('attorney doesn't exist', getByAttorney(1236, chuck, 35, Wyoming){
+		assert.equal(get(formId);
+	});
+});
+
+#placeholder for when I figure out how to deal with unions in this context
