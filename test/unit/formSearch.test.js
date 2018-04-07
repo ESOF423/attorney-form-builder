76b72dc479b231b75ba589bf7dec333e84606484
Stephanie McLaren <smclaren05@gmail.com>
2018-04-06 21:58:19 -0600

    Update purchaseForm.test.js
    Fixed purchaseForm test and updated errors in formSearch.test.js

--- test/unit/formSearch.test.js
+++ test/unit/formSearch.test.js
@@ -19,51 +19,53 @@ after(async () => {
 describe('formSearch test', async () => {
     it('should find', async () => {
         await page.goto('${server}/formSearch')
-        await page.name('#name','Will')
-        await page.attorney('#attorney','Bill Waterson')
-        await page.cost('4')
-        
+        await page.type('#name','Will')
+        await page.type('#attorney','Bill Waterson')
+        await page.type('4')
+        await page.click('#formSearch input[type=submit]')
+        await page.waitFor(1000)
+    
         var successText = await page.$eval("#successText", el => el.innerHTML);
 
     expect(successText).to.equal("Success")
     }).timeout(10000);
-})
 
-describe('invalid attorny test', async () => {
     it('should not find', async () => {
         await page.goto('${server}/formSearch')
-        await page.name('#name', 'Will')
-        await page.name('#attorney', 'invalid')
-        await page.cost('4')
+        await page.type('#name', 'Will')
+        await page.type('#attorney', 'invalid')
+        await page.type('4')
+        await page.click('#formSearch input[type=submit]')
+        await page.waitFor(1000)
 
         var errorText = await page.$eval("#errorText", el => el.innerHTML);
 
         expect(errorText).to.equal("No forms for submitted attorney")
     }).timeout(10000);
-})
 
-describe('invalid name test', async () => {
     it('should not find', async () => {
         await page.goto('${server}/formSearch')
-        await page.name('#name', 'invalid')
-        await page.name('#attorney', 'Bill Waterson')
-        await page.cost('4')
-
+        await page.type('#name', 'invalid')
+        await page.type('#attorney', 'Bill Waterson')
+        await page.type('4')
+        await page.click('#formSearch input[type=submit]')
+        await page.waitFor(1000)
+        
         var errorText = await page.$eval("#errorText", el => el.innerHTML);
 
         expect(errorText).to.equal("Invalid form name")
     }).timeout(10000);
-})
 
-describe('invalid cost test', async () => {
     it('should not find', async () => {
         await page.goto('${server}/formSearch')
-        await page.name('#name', 'Will')
-        await page.name('#attorney', 'Legal Beagle')
-        await page.cost('invalid')
+        await page.type('#name', 'Will')
+        await page.type('#attorney', 'Legal Beagle')
+        await page.type('invalid')
+        await page.click('#formSearch input[type=submit]')
+        await page.waitFor(1000)
 
         var errorText = await page.$eval("#errorText", el => el.innerHTML);
 
         expect(errorText).to.equal("Invalid form cost")
     }).timeout(10000);
-})
+});
