5e615401a589b9768ee83f86252564cb9fcafc9f
Stephanie McLaren <smclaren05@gmail.com>
2018-04-08 16:55:07 -0600

    Cleaned up test folder structure
    Removed test/test  and test/test/e2e folder structure.

--- test/test/index.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-const express = require('express')
-const router = express.Router()
-
-router.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../views', 'index.html'))
-})
-
-module.exports = router
